0656f885037727e31b5f2c8abc5ddab5818a4ff8
AnastasiaV-01 <145901482+AnastasiaV-01@users.noreply.github.com>
2023-10-23 13:41:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -63,3 +63,106 @@
 // {
 //     System.Console.WriteLine("Нет");
 // }
+
+
+
+
+
+// УРОК 4. ФУНКЦИИ
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int numberA = ReadInt("Введите число: ");
+// int numberB = ReadInt("Введите степень: ");
+// Degree(numberA, numberB);
+
+//  void Degree(int a, int N)
+//  {
+//    int result = 1;
+//    for(int i = 1; i <= N; i++)
+//    {
+//     result = result * a;
+//    }
+//    System.Console.WriteLine(a + " в степени " + N + " = " + result); 
+//  }
+// int ReadInt(string message)
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int number = ReadInt("Введите число: ");
+
+// int len = NumberLen(number);
+// SumNumbers(number, len);
+
+// // Функция подсчета цифр в числе
+// int NumberLen(int a)
+// {
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+
+// // Функция вывода суммы цифр в числе
+// void SumNumbers(int n, int len)
+// {
+//     int sum = 0;
+//     for (int i=1; i <= len ;i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine($"сумма цифр {sum}");
+// }
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int LenArray = ReadInt("Введите длинну массива: ");
+// int[] RandomArray = new int[LenArray];
+// {
+//     for(int i = 0; i < RandomArray.Length; i++)
+//     {
+//         RandomArray[i] = new Random().Next(1,99);
+//         System.Console.Write(RandomArray[i] + " ");
+//     }
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
