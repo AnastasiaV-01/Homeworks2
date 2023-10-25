acd701ab6286cec84d2661438ca5d4b3c5389ee2
AnastasiaV-01 <145901482+AnastasiaV-01@users.noreply.github.com>
2023-10-26 00:40:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,80 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+// Урок 6. Одномерные массивы. Продолжение
 
-// System.Console.Write("Input number: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if(num >= 100 && num <= 999)
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// System.Console.Write("Введите количество элементов массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[m];
+
+// void massiv(int m)
 // {
-//     System.Console.WriteLine($"{num} -> {(num / 10) % 10}");
+//   for (int i = 0; i < m; i++)
+//    {
+//      System.Console.WriteLine($"Введите {i+1} элемент массива ");
+//      Array[i] = Convert.ToInt32(Console.ReadLine());
+//    }
 // }
-// else
+
+// int quantity(int[] Array)
 // {
-//     System.Console.WriteLine("You input not three-digit number");
+//   int i = 0;
+//   int sum = 0;
+//   while (i < Array.Length)
+//    {
+//      if(Array[i]>0)
+//     sum = sum + 1;
+//     i = i + 1;
+//    }
+//    return sum;
 // }
 
+// massiv(m);
+// System.Console.Write($"\n Чисел больше нуля: {quantity(Array)}");
 
 
 
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// System.Console.Write("Input number: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if(num >= 100 && num <= 999)
-// {
-//     System.Console.WriteLine($"{num} -> {num % 10}");
-// }
-// else
-// {
-//     System.Console.WriteLine("Третьей цифры нет");
-// }
+// Console.WriteLine("Введите переменную b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = ((b2 - b1) / (k1 - k2));
+// double y = k1 * x + b1;
+
+// Console.Write($"\n Точка пересечения двух прямых: [{x},{y}]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
